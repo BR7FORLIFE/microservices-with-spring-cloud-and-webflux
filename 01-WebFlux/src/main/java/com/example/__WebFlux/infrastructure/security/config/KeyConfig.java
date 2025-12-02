c52a14ce8179fc1734a0eb89d5a2f8345d6427a3
br7forlife <diazpachecobrayan@gmail.com>
2025-12-02 15:35:04 -0500

    add documentation

--- 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/config/KeyConfig.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/config/KeyConfig.java
@@ -9,6 +9,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KeyConfig {
 
+    /**
+     * 
+     * @return
+     * @throws Exception
+     * 
+     *                   Esta configuracion no funciona en entornos de produccion ya
+     *                   que cada vez
+     *                   que se recompila el proyecto las claves privadas y publicas
+     *                   cambian
+     */
     @Bean
     public KeyPair keyPair() throws Exception {
         KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
