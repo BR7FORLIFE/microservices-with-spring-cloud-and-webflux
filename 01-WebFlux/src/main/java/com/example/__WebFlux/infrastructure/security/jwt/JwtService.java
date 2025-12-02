942ec4f9a5237ed2e6febe8e8be07b252a358514
br7forlife <diazpachecobrayan@gmail.com>
2025-12-02 15:36:16 -0500

    finished jwtservice

--- 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/jwt/JwtService.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/jwt/JwtService.java
@@ -3,18 +3,40 @@ package com.example.__WebFlux.infrastructure.security.jwt;
 import java.security.KeyPair;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
-import reactor.core.publisher.Mono;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 
-@RequiredArgsConstructor
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
 @Service
 public class JwtService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private RSAPrivateKey privateKey;
+
+    @Autowired
     private RSAPublicKey publicKey;
+
     private String issuer = "";
     private Integer accessTokenSeconds = 60 * 60 * 60;
 
@@ -23,7 +45,89 @@ public class JwtService {
         this.privateKey = (RSAPrivateKey) keyPair.getPrivate();
     }
 
-    public Mono<String> generateAccessToken() {
-        return Mono.just(null);
+    // Genera el Jwt Con nimbus jose ademas se generan aleatoriamente las keys
+    public Mono<String> generateAccessToken(UserDetails userDetails) {
+        return Mono.fromCallable(() -> {
+            Instant timestampCurrent = Instant.now();
+            Instant timestampFuture = timestampCurrent.plusSeconds(this.accessTokenSeconds);
+
+            Date currentDate = Date.from(timestampCurrent);
+            Date expirationDate = Date.from(timestampFuture);
+
+            /**
+             * Caracteristicas de Nimbus JOSE
+             * 
+             * - Crea el JWT por sus 3 piezas fundamentales (Header, Payload, Signature)
+             */
+
+            // Payload del token -> Nimbus JOSE
+            JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                    .issuer(this.issuer)
+                    .subject(passwordEncoder.encode(userDetails.getUsername()))
+                    .issueTime(currentDate)
+                    .expirationTime(expirationDate)
+                    .claim("ROLS", userDetails.getAuthorities())
+                    .jwtID(UUID.randomUUID().toString())
+                    .build();
+
+            // Header del token -> Nimbus JOSE
+            JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS256)
+                    .type(JOSEObjectType.JWT)
+                    .build();
+
+            // Unimos las partes header - payload !Sin firmar
+            SignedJWT signedJWT = new SignedJWT(header, claimsSet);
+
+            // Firmamos segun el metodo de encryptacion definido (RSA256)
+            RSASSASigner signer = new RSASSASigner(this.privateKey);
+            signedJWT.sign(signer);
+
+            // Devolvemos el Jwt serializado en BASE64
+            return signedJWT.serialize();
+        }).subscribeOn(Schedulers.boundedElastic());
+    }
+
+    /**
+     * Etapas de validaciones de JWT
+     * 
+     * 1. Validacion cryptografica (clave publica y clave privada)
+     * 2. Tiempos (tiempo de expiracion emitido en el token)
+     * 3. JWT ID (para hacer revocaciones)
+     */
+
+    public Mono<JWTClaimsSet> validateAccessToken(String token) throws JOSEException {
+        return Mono.fromCallable(() -> {
+            // parseamos el token en sus 3 partes (header, payload, signature)
+            SignedJWT signedJWT = SignedJWT.parse(token);
+
+            /**
+             * Validacion Cryptografica
+             */
+
+            // objeto encargado de verificar la firma
+            JWSVerifier jwsVerifier = new RSASSAVerifier(this.publicKey);
+
+            // validamos si conserva su integridad
+            if (!signedJWT.verify(jwsVerifier)) {
+                throw new JOSEException("invalid signature");
+            }
+
+            // obtenemos las claims
+            JWTClaimsSet claims = signedJWT.getJWTClaimsSet();
+
+            // Validacion por tiempos
+            Date date = new Date();
+
+            if (claims.getExpirationTime() == null || date.after(claims.getExpirationTime())) {
+                throw new Exception("Token expired!");
+            }
+
+            return claims;
+        }).subscribeOn(Schedulers.boundedElastic());
+    }
+
+    // retornaar el Public Key
+    public RSAPublicKey getPublicKey() {
+        return this.publicKey;
     }
 }
