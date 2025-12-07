aa03947b7e09966526931151b5ee5279ba23bfa9
br7forlife <diazpachecobrayan@gmail.com>
2025-12-06 19:13:14 -0500

    redified the service

--- 01-WebFlux/src/main/java/com/example/__WebFlux/domain/refreshToken/services/RefreshTokenDomainService.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/domain/refreshToken/services/RefreshTokenDomainService.java
@@ -4,11 +4,11 @@ import java.time.Instant;
 
 import com.example.__WebFlux.domain.refreshToken.exceptions.RefreshTokenExpiredException;
 import com.example.__WebFlux.domain.refreshToken.exceptions.RefreshTokenRevockedException;
-import com.example.__WebFlux.domain.refreshToken.models.RefreshToken;
+import com.example.__WebFlux.domain.refreshToken.models.RefreshTokenModel;
 
 public class RefreshTokenDomainService {
 
-    public void validateToken(RefreshToken token, Instant now) {
+    public void validateToken(RefreshTokenModel token, Instant now) {
         if (token.isRevoked()) {
             throw new RefreshTokenRevockedException();
         }
@@ -18,7 +18,7 @@ public class RefreshTokenDomainService {
         }
     }
 
-    public RefreshToken revoke(RefreshToken token){
-        return token.markRevoked();
+    public RefreshTokenModel revoke(RefreshTokenModel token){
+        return token.revokedCopy();
     } 
 }
