1b1238f75c1e67a5cc2c2c9ea37a5f28421b34ea
br7forlife <diazpachecobrayan@gmail.com>
2025-12-09 17:08:16 -0500

    rename the archive for more lecture

---
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/domain/refreshToken/ports/RefreshTokenDomainRepositoryPort.java
@@ -0,0 +1,16 @@
+package com.example.__WebFlux.domain.refreshToken.ports;
+
+import com.example.__WebFlux.domain.refreshToken.models.RefreshTokenModel;
+
+import reactor.core.publisher.Mono;
+
+public interface RefreshTokenDomainRepositoryPort {
+
+    Mono<RefreshTokenModel> findByTokenHash(String tokenHash);
+
+    Mono<RefreshTokenModel> findById(String id);
+
+    Mono<RefreshTokenModel> findByUserIdAndNotRevoked(String userId);
+
+    Mono<RefreshTokenModel> save(RefreshTokenModel token);
+}
