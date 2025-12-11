753ec4d3566898f37e9f03a7117e72a3e0ff1b66
br7forlife <diazpachecobrayan@gmail.com>
2025-12-11 11:46:22 -0500

    create the repository for postgres and add some query methods

---
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/refreshToken/repository/postgres/SpringDataRefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.example.__WebFlux.infrastructure.refreshToken.repository.postgres;
+
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import com.example.__WebFlux.infrastructure.refreshToken.persistence.RefreshTokenEntity;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface SpringDataRefreshTokenRepository extends ReactiveCrudRepository<RefreshTokenEntity, String> {
+
+    @Query("SELECT * FROM refresh_token WHERE token_hash = :token")
+    Mono<RefreshTokenEntity> findByTokenHash(String token);
+
+    Flux<RefreshTokenEntity> findByUserIdAndNotRevoked(String userId);
+}
