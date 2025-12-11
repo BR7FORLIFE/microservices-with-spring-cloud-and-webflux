d9aea5a8143fd0ffd450346533072847d0f096c9
br7forlife <diazpachecobrayan@gmail.com>
2025-12-11 11:45:43 -0500

    defined R2DBC captaer with spring

--- 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/refreshToken/repository/R2dbcRefreshTokenRepositoryAdapter.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/refreshToken/repository/R2dbcRefreshTokenRepositoryAdapter.java
@@ -1,29 +1,50 @@
 package com.example.__WebFlux.infrastructure.refreshToken.repository;
 
+import org.springframework.stereotype.Component;
+
 import com.example.__WebFlux.domain.refreshToken.models.RefreshTokenModel;
 import com.example.__WebFlux.domain.refreshToken.ports.RefreshTokenDomainRepositoryPort;
+import com.example.__WebFlux.infrastructure.refreshToken.mapper.RefreshTokenMapper;
+import com.example.__WebFlux.infrastructure.refreshToken.repository.postgres.SpringDataRefreshTokenRepository;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Component
 public class R2dbcRefreshTokenRepositoryAdapter implements RefreshTokenDomainRepositoryPort {
+
+    private final SpringDataRefreshTokenRepository refreshTokenRepository;
+
+    public R2dbcRefreshTokenRepositoryAdapter(SpringDataRefreshTokenRepository repository){
+        this.refreshTokenRepository = repository;
+    }
 
     @Override
     public Mono<RefreshTokenModel> findById(String id) {
-        return null;
+        return refreshTokenRepository
+                .findById(id)
+                .map(RefreshTokenMapper::toDomain);
     }
 
     @Override
     public Mono<RefreshTokenModel> findByTokenHash(String tokenHash) {
-        return null;
+        return refreshTokenRepository
+                .findByTokenHash(tokenHash)
+                .map(RefreshTokenMapper::toDomain);
     }
 
     @Override
-    public Mono<RefreshTokenModel> findByUserIdAndNotRevoked(String userId) {
-        return null;
+    public Flux<RefreshTokenModel> findByUserIdAndNotRevoked(String userId) {
+        return refreshTokenRepository
+                .findByUserIdAndNotRevoked(userId)
+                .map(RefreshTokenMapper::toDomain);
     }
 
     @Override
-    public Mono<RefreshTokenModel> save(RefreshTokenModel token) {
-        return null;
+    public Mono<RefreshTokenModel> save(RefreshTokenModel tokenModel) {
+        return refreshTokenRepository
+                .save(RefreshTokenMapper.toEntity(tokenModel))
+                .map(RefreshTokenMapper::toDomain);
     }
+
 }
