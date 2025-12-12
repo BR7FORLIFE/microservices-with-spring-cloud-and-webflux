43c809f9e8bd875d49dc206801dd157633eb0de9
br7forlife <diazpachecobrayan@gmail.com>
2025-12-12 11:17:02 -0500

    create the authController

---
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.example.__WebFlux.infrastructure.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    
+}
