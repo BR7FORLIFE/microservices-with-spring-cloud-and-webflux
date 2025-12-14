fadc78ccd4f00d7faf8a2649e39dca29caf0cd53
br7forlife <diazpachecobrayan@gmail.com>
2025-12-14 16:00:50 -0500

    feat: create the login request and response dtos

---
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/application/security/dtos/request/LoginUserRequestDto.java
@@ -0,0 +1,5 @@
+package com.example.__WebFlux.application.security.dtos.request;
+
+public record LoginUserRequestDto(String username, String email ,String password) {
+
+}
---
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/application/security/dtos/response/LoginUserResponseDto.java
@@ -0,0 +1,5 @@
+package com.example.__WebFlux.application.security.dtos.response;
+
+public record LoginUserResponseDto(String access_token) {
+    
+}
