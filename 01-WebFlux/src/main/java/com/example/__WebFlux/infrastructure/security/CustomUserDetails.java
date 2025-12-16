7e69cc33ae0ff94cd9952a6f0892733bba9815fe
br7forlife <diazpachecobrayan@gmail.com>
2025-12-15 19:33:02 -0500

    refactor: change some atributies and defined the logic in the constructor

--- 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/CustomUserDetails.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/CustomUserDetails.java
@@ -3,30 +3,41 @@ package com.example.__WebFlux.infrastructure.security;
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.example.__WebFlux.infrastructure.security.persistence.UserModel;
+import com.example.__WebFlux.domain.user.models.UserModel;
 
 public class CustomUserDetails implements UserDetails {
 
-    private UserModel userModel;
+    private String userId;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
 
     public CustomUserDetails(UserModel userModel) {
-        this.userModel = userModel;
+        this.userId = userModel.getId();
+        this.username = userModel.getUsername();
+        this.password = userModel.getPassword();
+        this.authorities = userModel.getRols().stream().map(SimpleGrantedAuthority::new).toList();
+    }
+
+    public String getUserId() {
+        return this.userId;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return this.authorities;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.username;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return this.password;
     }
 }
