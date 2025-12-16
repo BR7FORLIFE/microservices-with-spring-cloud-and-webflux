434cd190cb52863d363fca37f61c012d01d93be7
br7forlife <diazpachecobrayan@gmail.com>
2025-12-15 19:32:22 -0500

    fix: add @component for add in the application context

--- 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/BearerTokenServerAuthenticationConverter.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/BearerTokenServerAuthenticationConverter.java
@@ -4,10 +4,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
 
+@Component
 public class BearerTokenServerAuthenticationConverter implements ServerAuthenticationConverter {
 
     @Override
