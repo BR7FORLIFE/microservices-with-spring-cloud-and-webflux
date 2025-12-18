1701baf6f06856600a099061c4dad14e7a6745ad
br7forlife <diazpachecobrayan@gmail.com>
2025-12-18 15:22:32 -0500

    fix: change the userModel to UsermodelDomain

--- 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/repository/postgres/SpringDataUserRepository.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/repository/postgres/SpringDataUserRepository.java
@@ -2,11 +2,11 @@ package com.example.__WebFlux.infrastructure.security.repository.postgres;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-import com.example.__WebFlux.infrastructure.security.persistence.UserModel;
+import com.example.__WebFlux.infrastructure.security.persistence.UserModelEntity;
 
 import reactor.core.publisher.Mono;
 
-public interface SpringDataUserRepository extends ReactiveCrudRepository<UserModel, String>{
-    Mono<UserModel> findByUsername(String username);
-    Mono<UserModel> findByEmail(String email);
+public interface SpringDataUserRepository extends ReactiveCrudRepository<UserModelEntity, String>{
+    Mono<UserModelEntity> findByUsername(String username);
+    Mono<UserModelEntity> findByEmail(String email);
 }
