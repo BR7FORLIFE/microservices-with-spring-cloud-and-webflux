16867766554edda08a34aa06d778a32b7f559db3
br7forlife <diazpachecobrayan@gmail.com>
2025-12-18 15:17:41 -0500

    refactor: change the UserModel to UserModelDomain

--- 01-WebFlux/src/main/java/com/example/__WebFlux/domain/user/ports/UserDomainRepositoryPort.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/domain/user/ports/UserDomainRepositoryPort.java
@@ -1,10 +1,10 @@
 package com.example.__WebFlux.domain.user.ports;
 
-import com.example.__WebFlux.domain.user.models.UserModel;
+import com.example.__WebFlux.domain.user.models.UserModelDomain;
 
 import reactor.core.publisher.Mono;
 
 public interface UserDomainRepositoryPort {
-    Mono<UserModel> findByUsername(String username);
-    Mono<UserModel> findByEmail(String email);
+    Mono<UserModelDomain> findByUsername(String username);
+    Mono<UserModelDomain> findByEmail(String email);
 }
