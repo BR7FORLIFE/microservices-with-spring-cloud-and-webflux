c257e05aa2daee6801538bc1e4d456518b927751
br7forlife <diazpachecobrayan@gmail.com>
2025-12-18 15:19:35 -0500

    fix: add service decorator

--- 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/CustomReactiveUserDetailsService.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/CustomReactiveUserDetailsService.java
@@ -2,11 +2,13 @@ package com.example.__WebFlux.infrastructure.security;
 
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import com.example.__WebFlux.infrastructure.security.repository.R2dbcUserRepositoryAdapter;
 
 import reactor.core.publisher.Mono;
 
+@Service
 public class CustomReactiveUserDetailsService implements ReactiveUserDetailsService {
 
     private final R2dbcUserRepositoryAdapter r2dbcUserRepositoryAdapter;
