a7735115ddbae7f07f74a8a46a6b26a1b9fa611d
br7forlife <diazpachecobrayan@gmail.com>
2025-12-18 15:22:02 -0500

    feat: create the r2dbc repository and some methods

--- 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/repository/R2dbcUserRepositoryAdapter.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/security/repository/R2dbcUserRepositoryAdapter.java
@@ -1,27 +1,33 @@
 package com.example.__WebFlux.infrastructure.security.repository;
 
-import com.example.__WebFlux.domain.user.models.UserModel;
+import org.springframework.stereotype.Repository;
+
+import com.example.__WebFlux.domain.user.models.UserModelDomain;
 import com.example.__WebFlux.domain.user.ports.UserDomainRepositoryPort;
+import com.example.__WebFlux.infrastructure.security.mapper.UserMapper;
 import com.example.__WebFlux.infrastructure.security.repository.postgres.SpringDataUserRepository;
 
 import reactor.core.publisher.Mono;
 
-public class R2dbcUserRepositoryAdapter  implements UserDomainRepositoryPort {
+@Repository
+public class R2dbcUserRepositoryAdapter implements UserDomainRepositoryPort {
 
     private final SpringDataUserRepository userRepository;
 
-    public R2dbcUserRepositoryAdapter(SpringDataUserRepository springDataUserRepository){
+    public R2dbcUserRepositoryAdapter(SpringDataUserRepository springDataUserRepository) {
         this.userRepository = springDataUserRepository;
     }
 
     @Override
-    public Mono<UserModel> findByUsername(String username) {
-        return null;
+    public Mono<UserModelDomain> findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .map(UserMapper::toDomain);
     }
 
     @Override
-    public Mono<UserModel> findByEmail(String email) {
-        return null;
+    public Mono<UserModelDomain> findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .map(UserMapper::toDomain);
     }
-    
+
 }
