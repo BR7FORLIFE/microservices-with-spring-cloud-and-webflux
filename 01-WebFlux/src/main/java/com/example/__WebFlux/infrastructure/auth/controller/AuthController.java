400b6401e6a40cffed13b6846b1fe7a722c7fcd9
br7forlife <diazpachecobrayan@gmail.com>
2025-12-21 12:06:59 -0500

    feat: terminated the register endpoint

--- 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/auth/controller/AuthController.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/auth/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.example.__WebFlux.infrastructure.auth.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
@@ -13,8 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.__WebFlux.application.auth.command.RegisterUserCommand;
 import com.example.__WebFlux.application.auth.dtos.request.LoginUserRequestDto;
+import com.example.__WebFlux.application.auth.dtos.request.RegisterUserRequestDto;
 import com.example.__WebFlux.application.auth.dtos.response.LoginUserResponseDto;
+import com.example.__WebFlux.application.auth.dtos.response.RegisterUserResponseDto;
+import com.example.__WebFlux.application.auth.orchestator.AuthUseCaseImp;
 import com.example.__WebFlux.application.refreshToken.dtos.RefreshTokenDtoResponse;
 import com.example.__WebFlux.application.refreshToken.orchestrator.RefreshTokenUseCaseImp;
 import com.example.__WebFlux.infrastructure.security.CustomUserDetails;
@@ -31,18 +37,25 @@ public class AuthController {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final RefreshTokenUseCaseImp refreshTokenUseCaseImp;
+    private final AuthUseCaseImp authUseCaseImp;
 
     public AuthController(ReactiveUserDetailsService reactiveUserDetailsService, PasswordEncoder passwordEncoder,
-            JwtService jwtService, RefreshTokenUseCaseImp refreshTokenUseCaseImp) {
+            JwtService jwtService, RefreshTokenUseCaseImp refreshTokenUseCaseImp, AuthUseCaseImp authUseCaseImp) {
         this.reactiveUserDetailsService = reactiveUserDetailsService;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
         this.refreshTokenUseCaseImp = refreshTokenUseCaseImp;
+        this.authUseCaseImp = authUseCaseImp;
     }
 
     @PostMapping("/register")
-    public Mono<ResponseEntity<?>> register() {
-        return null;
+    public Mono<ResponseEntity<RegisterUserResponseDto>> register(@RequestBody @Valid RegisterUserRequestDto user) {
+        RegisterUserCommand cmd = new RegisterUserCommand(user.username(), user.password(), user.email());
+
+        return authUseCaseImp.execute(cmd)
+                .map(result -> ResponseEntity.ok()
+                        .body(new RegisterUserResponseDto(result.user_id(), result.username())))
+                .onErrorResume(e -> Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).build()));
     }
 
     @PostMapping("/login")
@@ -58,7 +71,7 @@ public class AuthController {
                         return Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
                     }
                     CustomUserDetails customUserDetails = (CustomUserDetails) user;
-                    String user_id = customUserDetails.getUserId();
+                    UUID user_id = customUserDetails.getUserId();
                     return jwtService.generateAccessToken(user)
                             .flatMap(access_token -> refreshTokenUseCaseImp.createRefreshToken(user_id)
                                     .map(refreshRaw -> {
