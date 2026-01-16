dc062c0669fe6dc2650d249617e029adc16b5b14
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-16 14:19:16 -0500

    feat: add verifyUser in Auth use case

--- 01-WebFlux/src/main/java/com/example/__WebFlux/application/auth/usecases/AuthUseCase.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/application/auth/usecases/AuthUseCase.java
@@ -1,10 +1,14 @@
 package com.example.__WebFlux.application.auth.usecases;
 
+import java.util.UUID;
+
 import com.example.__WebFlux.application.auth.command.RegisterUserCommand;
 import com.example.__WebFlux.application.auth.command.RegisterUserCommandResult;
+import com.example.__WebFlux.application.auth.command.VerifiedUserCommandResult;
 
 import reactor.core.publisher.Mono;
 
 public interface AuthUseCase {
     Mono<RegisterUserCommandResult> execute(RegisterUserCommand cmd);
+    Mono<VerifiedUserCommandResult> verifyUser(UUID userId);
 }
