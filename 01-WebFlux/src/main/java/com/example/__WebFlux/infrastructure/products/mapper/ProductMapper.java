c7457f5318431b5dcb349ff103251090e4e96158
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-16 14:28:41 -0500

    feat: create the mapper and entity for product

---
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/infrastructure/products/mapper/ProductMapper.java
@@ -0,0 +1,29 @@
+package com.example.__WebFlux.infrastructure.products.mapper;
+
+import com.example.__WebFlux.domain.products.models.ProductModelDomain;
+import com.example.__WebFlux.infrastructure.products.persistence.ProductEntity;
+
+public class ProductMapper {
+
+    public static ProductModelDomain toDomain(ProductEntity productEntity) {
+        return new ProductModelDomain(
+                productEntity.getProductId(),
+                productEntity.getSku(),
+                productEntity.getName(),
+                productEntity.getShortDescription(),
+                productEntity.getLongDescription(),
+                productEntity.getModel());
+    }
+
+    public static ProductEntity toEntity(ProductModelDomain productModelDomain) {
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setProductId(productModelDomain.getProductId());
+        productEntity.setSku(productModelDomain.getSku());
+        productEntity.setName(productModelDomain.getName());
+        productEntity.setShortDescription(productModelDomain.getShortDescription());
+        productEntity.setLongDescription(productModelDomain.getLongDescription());
+        productEntity.setModel(productModelDomain.getModel());
+
+        return productEntity;
+    }
+}
