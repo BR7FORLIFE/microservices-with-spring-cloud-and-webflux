af0978c62d66875493570e4c00cfcd1463e8a7dd
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-16 14:22:48 -0500

    fix: remove the exists by user for improve the logic

--- 01-WebFlux/src/main/java/com/example/__WebFlux/domain/auth/ports/UserDomainRepositoryPort.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/domain/auth/ports/UserDomainRepositoryPort.java
@@ -1,12 +1,17 @@
 package com.example.__WebFlux.domain.auth.ports;
 
+import java.util.UUID;
+
 import com.example.__WebFlux.domain.auth.models.UserModelDomain;
 
 import reactor.core.publisher.Mono;
 
 public interface UserDomainRepositoryPort {
+    Mono<UserModelDomain> findByUserId(UUID id);
+
     Mono<UserModelDomain> findByUsername(String username);
+
     Mono<UserModelDomain> findByEmail(String email);
-    Mono<Boolean> existsByUsername(String username);
+
     Mono<UserModelDomain> save(UserModelDomain userModelDomain);
 }
