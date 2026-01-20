08982c8e2319b2fef1cafa8d3acfb1b5fdb05ccf
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-20 18:48:47 -0500

    feat: port for product

--- 01-WebFlux/src/main/java/com/example/__WebFlux/domain/products/ports/ProductDomainRepositoryPort.java
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/domain/products/ports/ProductDomainRepositoryPort.java
@@ -1,5 +1,15 @@
 package com.example.__WebFlux.domain.products.ports;
 
-public class ProductDomainRepositoryPort {
-    
+import java.util.UUID;
+
+import com.example.__WebFlux.domain.products.models.ProductModelDomain;
+
+import reactor.core.publisher.Mono;
+
+public interface ProductDomainRepositoryPort {
+    Mono<ProductModelDomain> findByProductId(UUID productId);
+
+    Mono<ProductModelDomain> findBySku(String sku);
+
+    Mono<ProductModelDomain> save(ProductModelDomain product);
 }
