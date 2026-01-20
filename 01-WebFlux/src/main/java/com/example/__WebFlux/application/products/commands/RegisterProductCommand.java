0a4516a0e3928b8cd2fbef067f583ea2b26a1526
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-20 18:46:47 -0500

    feat: create the register product command and result

---
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/application/products/commands/RegisterProductCommand.java
@@ -0,0 +1,6 @@
+package com.example.__WebFlux.application.products.commands;
+
+public record RegisterProductCommand(String name, String sku, String shortDescription, String longDescription,
+        String model) {
+
+}
---
+++ 01-WebFlux/src/main/java/com/example/__WebFlux/application/products/commands/RegisterProductCommandResult.java
@@ -0,0 +1,5 @@
+package com.example.__WebFlux.application.products.commands;
+
+public record RegisterProductCommandResult(String productId, String name) {
+
+}
