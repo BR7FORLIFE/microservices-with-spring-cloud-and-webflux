394d4c80ffcce9f9abec63e9271889617f79afab
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-02-09 14:26:43 -0500

    feat: create assets media domain model and OwnerMediaType for define the differents author in the upload image

---
+++ 01-WebFlux/src/main/java/com/example/webflux/domain/AssetsMedia/models/AssetsMediaModel.java
@@ -0,0 +1,119 @@
+package com.example.webflux.domain.AssetsMedia.models;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public final class AssetsMediaModel {
+
+    public final UUID assetsMediaId;
+    public final UUID ownerId;
+    public final String ownerType;
+    public final String cloudPublicId;
+    public final String format;
+    public final String resourceType;
+    public final String storageType;
+    public final Integer numberBytes;
+    public final Integer width;
+    public final Integer height;
+    public final String url;
+    public final String secureUrl;
+    public final Integer position;
+    public final Boolean isCover;
+    public final Instant createAt;
+    public final Instant deleteAt;
+
+    private AssetsMediaModel(UUID assetsMediaId, UUID ownerId, String ownerType, String cloudPublicId, String format,
+            String resourceType, String storageType, Integer numberBytes, Integer width, Integer height, String url,
+            String secureUrl, Integer position, Boolean isCover, Instant createAt, Instant deleteAt) {
+        this.assetsMediaId = assetsMediaId;
+        this.ownerId = ownerId;
+        this.ownerType = ownerType;
+        this.cloudPublicId = cloudPublicId;
+        this.format = format;
+        this.resourceType = resourceType;
+        this.storageType = storageType;
+        this.numberBytes = numberBytes;
+        this.width = width;
+        this.height = height;
+        this.url = url;
+        this.secureUrl = secureUrl;
+        this.position = position;
+        this.isCover = isCover;
+        this.createAt = createAt;
+        this.deleteAt = deleteAt;
+    }
+
+    public static AssetsMediaModel createNew(UUID assetsMediaId, UUID ownerId, String ownerType, String cloudPublicId,
+            String format,
+            String resourceType, String storageType, Integer numberBytes, Integer width, Integer height, String url,
+            String secureUrl, Integer position, Boolean isCover, Instant createAt, Instant deleteAt) {
+
+        return new AssetsMediaModel(assetsMediaId, ownerId, ownerType, cloudPublicId, format, resourceType, storageType,
+                numberBytes, width, height, url, secureUrl, position, isCover, createAt, deleteAt);
+    }
+
+    public UUID getAssetsMediaId() {
+        return assetsMediaId;
+    }
+
+    public UUID getOwnerId() {
+        return ownerId;
+    }
+
+    public String getOwnerType() {
+        return ownerType;
+    }
+
+    public String getCloudPublicId() {
+        return cloudPublicId;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public String getStorageType() {
+        return storageType;
+    }
+
+    public Integer getNumberBytes() {
+        return numberBytes;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getSecureUrl() {
+        return secureUrl;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public Boolean getIsCover() {
+        return isCover;
+    }
+
+    public Instant getCreateAt() {
+        return createAt;
+    }
+
+    public Instant getDeleteAt() {
+        return deleteAt;
+    }
+
+}
---
+++ 01-WebFlux/src/main/java/com/example/webflux/domain/AssetsMedia/models/OwnerMediaType.java
@@ -0,0 +1,5 @@
+package com.example.webflux.domain.AssetsMedia.models;
+
+public enum OwnerMediaType {
+    USER, LISTING, PRODUCT
+}
