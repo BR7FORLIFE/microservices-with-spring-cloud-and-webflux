6a3a71814ce18afc25482368e06ed4d71f9623ec
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-02-15 19:38:10 -0500

    feat: create the jwt service adapter

---
+++ 01-WebFlux/src/main/java/com/example/webflux/infrastructure/security/adapters/UserJwtAdapter.java
@@ -0,0 +1,38 @@
+package com.example.webflux.infrastructure.security.adapters;
+
+import java.util.Set;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import com.example.webflux.application.auth.model.AuthenticatedUser;
+import com.example.webflux.application.auth.ports.UserJwtPort;
+import com.example.webflux.domain.auth.models.UserModelDomain;
+import com.example.webflux.infrastructure.security.CustomUserDetails;
+import com.example.webflux.infrastructure.security.jwt.JwtService;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class UserJwtAdapter implements UserJwtPort {
+
+    private final JwtService jwtService;
+
+    public UserJwtAdapter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public Mono<String> generateAccessToken(AuthenticatedUser user) {
+        UserDetails details = new CustomUserDetails(
+                new UserModelDomain(
+                        user.userId(),
+                        user.username(),
+                        null,
+                        user.password(),
+                        Set.copyOf(user.rols())));
+
+        return jwtService.generateAccessToken(details);
+    }
+
+}
