2f556fd08ac6ade065713708ddf1e9aa69fc94cb
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-02-15 19:36:22 -0500

    feat: create a model in the application layer for connect both worlds (infra / application)

---
+++ 01-WebFlux/src/main/java/com/example/webflux/application/auth/model/AuthenticatedUser.java
@@ -0,0 +1,13 @@
+package com.example.webflux.application.auth.model;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public record AuthenticatedUser(
+    UUID userId, 
+    String username,
+    String password,
+    Collection<String> rols    
+) {
+
+}
