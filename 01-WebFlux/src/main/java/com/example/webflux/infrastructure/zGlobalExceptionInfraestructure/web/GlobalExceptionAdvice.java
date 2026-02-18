ba83f07680b4d577c1be8effb781ad7acd8e7b42
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-02-18 14:08:35 -0500

    feat: create an exception for status auth email

--- 01-WebFlux/src/main/java/com/example/webflux/infrastructure/zGlobalExceptionInfraestructure/web/GlobalExceptionAdvice.java
+++ 01-WebFlux/src/main/java/com/example/webflux/infrastructure/zGlobalExceptionInfraestructure/web/GlobalExceptionAdvice.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.ServerWebInputException;
 
+import com.example.webflux.application.auth.exceptions.AuthStatusEmailVerified;
 import com.example.webflux.application.auth.exceptions.IncorrectPasswordException;
 import com.example.webflux.application.auth.exceptions.UserAlreadyRegisterException;
 import com.example.webflux.application.auth.exceptions.UserNotFoundException;
@@ -16,6 +17,15 @@ import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
+
+    // === EXCEPTIONES DE AUTENTICACION ===
+
+    @ExceptionHandler(AuthStatusEmailVerified.class)
+    public ResponseEntity<ApiError> handleEmailNotVerified(
+            AuthStatusEmailVerified ex,
+            ServerWebExchange exchange) {
+        return buildError(HttpStatus.NOT_ACCEPTABLE, ex.getMessage(), exchange);
+    }
 
     // Exepciones cuando el usuario esta registrado en la plataforma
     @ExceptionHandler(UserAlreadyRegisterException.class)
@@ -41,6 +51,8 @@ public class GlobalExceptionAdvice {
         return buildError(HttpStatus.FORBIDDEN, ex.getMessage(), exchange);
     }
 
+    // === HTTP ====
+
     // Excepciones cuando se manda en el cuerpo de la peticion un campo no permitido
     @ExceptionHandler(ServerWebInputException.class)
     public ResponseEntity<ApiError> handleMultipleFields(
@@ -54,6 +66,7 @@ public class GlobalExceptionAdvice {
         return buildError(HttpStatus.TOO_MANY_REQUESTS, "Datos invalidos!", exchange);
     }
 
+    // helper para los codigos de estados
     private ResponseEntity<ApiError> buildError(
             HttpStatus status,
             String message,
