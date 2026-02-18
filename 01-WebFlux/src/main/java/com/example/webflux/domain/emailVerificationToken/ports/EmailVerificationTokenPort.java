8bae4c88cc15017ff26f4bd85a07b2af75c45558
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-02-18 14:06:45 -0500

    feat: create port and controller file for email verification

---
+++ 01-WebFlux/src/main/java/com/example/webflux/domain/emailVerificationToken/ports/EmailVerificationTokenPort.java
@@ -0,0 +1,13 @@
+package com.example.webflux.domain.emailVerificationToken.ports;
+
+import java.util.UUID;
+
+import com.example.webflux.domain.emailVerificationToken.models.EmailVerificationTokenModel;
+
+import reactor.core.publisher.Mono;
+
+public interface EmailVerificationTokenPort {
+    Mono<EmailVerificationTokenModel> findById(UUID id);
+
+    Mono<EmailVerificationTokenModel> findByUserId(UUID userId);
+}
---
+++ 01-WebFlux/src/main/java/com/example/webflux/infrastructure/emailVerificationToken/controller/EmailVerificationTokenController.java
@@ -0,0 +1,11 @@
+package com.example.webflux.infrastructure.emailVerificationToken.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+public class EmailVerificationTokenController {
+
+    
+}
