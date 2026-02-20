237043e0aca5ffab11299e2a413ddcd5b1a1926d
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-02-20 14:30:48 -0500

    feat: define the send email logic

--- 01-WebFlux/src/main/java/com/example/webflux/application/emailVerificationToken/usecases/EmailVerifiedTokenUseCase.java
+++ 01-WebFlux/src/main/java/com/example/webflux/application/emailVerificationToken/usecases/EmailVerifiedTokenUseCase.java
@@ -1,5 +1,7 @@
 package com.example.webflux.application.emailVerificationToken.usecases;
 
+import com.example.webflux.application.emailVerificationToken.commands.SendEmailCommand;
+import com.example.webflux.application.emailVerificationToken.commands.SendEmailCommandResult;
 import com.example.webflux.application.emailVerificationToken.commands.VerifyEmailCommand;
 import com.example.webflux.application.emailVerificationToken.commands.VerifyEmailCommandResult;
 
@@ -7,4 +9,6 @@ import reactor.core.publisher.Mono;
 
 public interface EmailVerifiedTokenUseCase {
     Mono<VerifyEmailCommandResult> verifyEmail(VerifyEmailCommand cmd);
+
+    Mono<SendEmailCommandResult> sendEmail(SendEmailCommand cmd);
 }
