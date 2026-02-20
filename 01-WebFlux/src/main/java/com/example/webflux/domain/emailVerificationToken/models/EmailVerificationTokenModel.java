17e8d4731724cca36a1236388db17d4031d30894
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-02-20 14:33:15 -0500

    feat: create the consume logic for reiew the expired time, consume

--- 01-WebFlux/src/main/java/com/example/webflux/domain/emailVerificationToken/models/EmailVerificationTokenModel.java
+++ 01-WebFlux/src/main/java/com/example/webflux/domain/emailVerificationToken/models/EmailVerificationTokenModel.java
@@ -3,6 +3,9 @@ package com.example.webflux.domain.emailVerificationToken.models;
 import java.time.Instant;
 import java.util.UUID;
 
+import com.example.webflux.domain.emailVerificationToken.exceptions.EmailConsumedException;
+import com.example.webflux.domain.emailVerificationToken.exceptions.EmailExpiredException;
+
 public final class EmailVerificationTokenModel {
     private final UUID id;
     private final UUID userId;
@@ -21,9 +24,38 @@ public final class EmailVerificationTokenModel {
         this.createAt = createAt;
     }
 
-    public static EmailVerificationTokenModel createNew(UUID userId, String tokenHash, Instant expiredAt, Instant consumedAt,
+    public static EmailVerificationTokenModel createNew(UUID userId, String tokenHash, Instant expiredAt,
+            Instant consumedAt,
             Instant createAt) {
         return new EmailVerificationTokenModel(UUID.randomUUID(), userId, tokenHash, expiredAt, consumedAt, createAt);
+    }
+
+    // reglas de negocio que hay que implementar para veficar el email
+
+    /**
+     * 
+     * 1. Validar si el token de verificacion de email esta expirado
+     * 2. Validar si el token fue consumido
+     * 3. Activar el usuario si todo esta correcto
+     */
+    public EmailVerificationTokenModel consume(Instant now) {
+
+        if (this.expiredAt.isBefore(now)) {
+            throw new EmailExpiredException();
+        }
+
+        if (this.consumedAt != null) {
+            throw new EmailConsumedException();
+        }
+
+        return new EmailVerificationTokenModel(
+                this.id,
+                this.userId,
+                this.tokenHash,
+                this.expiredAt,
+                now,
+                this.createAt);
+
     }
 
     public UUID getId() {
