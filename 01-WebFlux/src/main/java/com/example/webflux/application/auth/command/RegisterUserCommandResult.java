56b1adf5d3d1c09955e9d941016021e94141d892
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-02-20 14:26:20 -0500

    feat: add message field for send the email response to client

--- 01-WebFlux/src/main/java/com/example/webflux/application/auth/command/RegisterUserCommandResult.java
+++ 01-WebFlux/src/main/java/com/example/webflux/application/auth/command/RegisterUserCommandResult.java
@@ -2,6 +2,6 @@ package com.example.webflux.application.auth.command;
 
 import java.util.UUID;
 
-public record RegisterUserCommandResult(UUID user_id, String username) {
+public record RegisterUserCommandResult(UUID user_id, String username, String message) {
 
 }
