f1845e2b11067bed1f5706e61ff51ea47c254cdd
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-02-20 14:32:09 -0500

    fix: update and use the hashconfig global for create the random raw token and hash functions

--- 01-WebFlux/src/main/java/com/example/webflux/application/refreshToken/orchestrator/RefreshTokenUseCaseImp.java
+++ 01-WebFlux/src/main/java/com/example/webflux/application/refreshToken/orchestrator/RefreshTokenUseCaseImp.java
@@ -1,11 +1,7 @@
 package com.example.webflux.application.refreshToken.orchestrator;
 
-import java.security.MessageDigest;
-import java.security.SecureRandom;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Base64;
-import java.util.HexFormat;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -16,7 +12,7 @@ import com.example.webflux.application.refreshToken.usecases.RefreshTokenUseCase
 import com.example.webflux.domain.refreshToken.models.RefreshTokenModel;
 import com.example.webflux.domain.refreshToken.ports.RefreshTokenDomainRepositoryPort;
 import com.example.webflux.domain.refreshToken.services.RefreshTokenDomainService;
-import com.nimbusds.jose.util.StandardCharset;
+import com.example.webflux.infrastructure.config.HashService;
 
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -25,17 +21,16 @@ import reactor.util.function.Tuples;
 @Service
 public class RefreshTokenUseCaseImp implements RefreshTokenUseCase {
 
+    private final HashService hashConfig;
     private final RefreshTokenDomainRepositoryPort repository; // <-- puerto del dominio de la feature RefreshToken
     private final Duration ttlDuration; // <-- representa una cantidad de tiempo ej: 5 Min, 30 Days, 90 seconds
-    private final SecureRandom secureRandom = new SecureRandom(); // <- generador de numeros aleatorios con gran
-                                                                  // entropia
 
-    public RefreshTokenUseCaseImp(RefreshTokenDomainRepositoryPort repo,
+    public RefreshTokenUseCaseImp(HashService hashConfig, RefreshTokenDomainRepositoryPort repo,
             @Value("${refresh-token.ttl}") Duration duration) {
+        this.hashConfig = hashConfig;
         this.repository = repo;
         this.ttlDuration = duration;
     }
-
     // creamos el refreshToken
     /**
      * Nos basamos de la entropia para generar el RawToken
@@ -45,8 +40,8 @@ public class RefreshTokenUseCaseImp implements RefreshTokenUseCase {
         return repository.revokeAllByUserId(userId)
                 .then(Mono.fromCallable(() -> {
                     UUID jti = UUID.randomUUID(); // <-- id unico de refreshToken Model
-                    String raw = this.randomBase64(32); // <-- Raw que devolvemos en el mono Base64 con alta entropia
-                    String hash = this.sha256(raw); // <-- Obligatorio hasearlo para seguridad por si roban la DB
+                    String raw = hashConfig.randomBase64(32); // <-- Raw que devolvemos en el mono Base64 con alta entropia
+                    String hash = hashConfig.sha256(raw); // <-- Obligatorio hasearlo para seguridad por si roban la DB
                     Instant now = Instant.now();
                     Instant expiredAt = now.plus(ttlDuration);
                     RefreshTokenModel refreshtokenModel = RefreshTokenModel.create(jti, userId, hash,
@@ -60,7 +55,7 @@ public class RefreshTokenUseCaseImp implements RefreshTokenUseCase {
     // recibe el raw token, busca por el hash, valida y rota el token
     @Override
     public Mono<String> validateAndRotate(String rawToken) {
-        String hash = this.sha256(rawToken);
+        String hash = hashConfig.sha256(rawToken);
         return repository.findByTokenHash(hash)
                 .switchIfEmpty(Mono.error(new ValidateAndRotateException()))
                 .flatMap(rt -> {
@@ -74,32 +69,8 @@ public class RefreshTokenUseCaseImp implements RefreshTokenUseCase {
     }
 
     public Mono<Void> revoke(String rawToken) {
-        String hash = this.sha256(rawToken);
+        String hash = hashConfig.sha256(rawToken);
         return repository.findByTokenHash(hash)
                 .flatMap(rt -> repository.save(rt.revoke())).then();
-    }
-
-    private String randomBase64(int bytes) {
-        byte[] b = new byte[bytes];
-
-        // asigna a cada bit de b un bit con entropia para usar
-        secureRandom.nextBytes(b);
-        /**
-         * Convierte a base64 especializado para url gracias a UrlEncoder
-         * 
-         * -> withoutPadding() -> es para eliminar el tipico ' = ' cuando es multiplo de
-         * 3
-         */
-        return Base64.getUrlEncoder().withoutPadding().encodeToString(b);
-    }
-
-    private String sha256(String value) {
-        try {
-            MessageDigest sha = MessageDigest.getInstance("SHA-256");
-            byte[] digest = sha.digest(value.getBytes(StandardCharset.UTF_8));
-            return HexFormat.of().formatHex(digest);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
