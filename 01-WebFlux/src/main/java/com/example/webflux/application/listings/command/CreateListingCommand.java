d2921c5334cad032da9be9e0f23daab2de09bbba
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-03-03 14:00:02 -0500

    feat: update CreateListingCommand and CreateListingRequestDto to use CurrencyEnum instead of String for currency

--- 01-WebFlux/src/main/java/com/example/webflux/application/listings/command/CreateListingCommand.java
+++ 01-WebFlux/src/main/java/com/example/webflux/application/listings/command/CreateListingCommand.java
@@ -3,8 +3,8 @@ package com.example.webflux.application.listings.command;
 import java.util.UUID;
 
 import com.example.webflux.application.listings.draft.ProductDraft;
+import com.example.webflux.domain.listings.models.CurrencyEnum;
 
-public record CreateListingCommand(UUID userId, ProductDraft product, Double price, String currency,
-        Boolean isActive) {
-    
+public record CreateListingCommand(UUID userId,ProductDraft product, Double price, CurrencyEnum currency) {
+
 }
